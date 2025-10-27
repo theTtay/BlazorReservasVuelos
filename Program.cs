5a629d8bf4a5a9317ee93894a614b43521093d01
stacy <crespinsb@gmail.com>
2025-10-27 10:08:17 -0600

    Agregué MudBlazor

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using BlazorReservasVuelos.Data;
 using BlazorReservasVuelos.Services;
 using BlazorReservasVuelos.Components;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddScoped<IVueloService, VueloService>();
 builder.Services.AddScoped<IDestinoService, DestinoService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
